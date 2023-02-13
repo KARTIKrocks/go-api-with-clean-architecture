e67ead8e01189410c5f75c387bc9e9c5d1482c00
KARTIKrocks <kartik.rajput622001@gmail.com>
2023-02-13 21:38:06 +0530

    added get post put patch delete in http/routes

--- http/chi-router.go
+++ http/chi-router.go
@@ -27,6 +27,18 @@ func (m *chiRouter) POST(uri string, f func(w http.ResponseWriter, r *http.Reque
 	chiDispatcher.Post(uri, f)
 }
 
+func (m *chiRouter) PUT(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	chiDispatcher.Put(uri, f)
+}
+
+func (m *chiRouter) PATCH(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	chiDispatcher.Patch(uri, f)
+}
+
+func (m *chiRouter) DELETE(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	chiDispatcher.Delete(uri, f)
+}
+
 func (m *chiRouter) SERVE(port string) {
 	fmt.Printf("CHi HTTP server running on port %v", port)
 	log.Fatal(http.ListenAndServe(port, chiDispatcher))
--- http/mux-router.go
+++ http/mux-router.go
@@ -27,6 +27,18 @@ func (m *muxRouter) POST(uri string, f func(w http.ResponseWriter, r *http.Reque
 	muxDispatcher.HandleFunc(uri, f).Methods("POST")
 }
 
+func (m *muxRouter) PUT(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	muxDispatcher.HandleFunc(uri, f).Methods("PUT")
+}
+
+func (m *muxRouter) PATCH(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	muxDispatcher.HandleFunc(uri, f).Methods("PATCH")
+}
+
+func (m *muxRouter) DELETE(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	muxDispatcher.HandleFunc(uri, f).Methods("DELETE")
+}
+
 func (m *muxRouter) SERVE(port string) {
 	fmt.Printf("Mux HTTP server running on port %v", port)
 	log.Fatal(http.ListenAndServe(port, muxDispatcher))
